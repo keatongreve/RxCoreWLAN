74fe1cc0744aaf10f4302672550a57409fc335a6
Keaton Greve <keatongreve@gmail.com>
2018-11-21 13:43:12 -0600

    Initial Commit

---
+++ RxNetworking/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  RxNetworking
+//
+//  Created by Keaton on 11/21/18.
+//  Copyright © 2018 Keaton. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ RxNetworking/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RxNetworking
+//
+//  Created by Keaton on 11/21/18.
+//  Copyright © 2018 Keaton. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
